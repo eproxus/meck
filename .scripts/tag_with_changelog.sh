52542769bcc4d99843d57de59e3ca44fb5e069af
Adam Lindberg <eproxus@gmail.com>
2011-07-20 16:25:55 +0200

    Uncomment dry run, document and clean up

--- .scripts/tag_with_changelog.sh
+++ .scripts/tag_with_changelog.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
+
+# Install for alias 'tag-cl' with:
+#   git config alias.tag-cl '!.scripts/tag_with_changelog.sh'
+
 set -e # Abort on first failure, so we don't mess something up
 
 if [ -z "$1" ]; then
+    # Missing tag name
     echo "usage: git tag-cl <tag>" >&2
     exit 129
 fi
 if [ ! -f CHANGELOG ]; then
+    # No changelog to be used
     echo "fatal: CHANGELOG missing" >&2
     exit 128
 fi
 if [ ! -z "$(git status)" ]; then
+    # Sanity check
     echo "fatal: dirty repository" >&2
     exit 128
 fi
 
 CHANGELOG=$(cat CHANGELOG)
 
-# Add tag with CHANGELOG and empty CHANGELOG afterwards
-echo echo "" > CHANGELOG
-echo sed -i "" -e "s/{vsn, .*}/{vsn, \"$1\"}/g" src/meck.app.src
+# Clean up changelog
+echo "" > CHANGELOG
+git add CHANGELOG
 
-echo git add src/meck.app.src
-echo git add CHANGELOG
+# Update version in .app file
+sed -i "" -e "s/{vsn, .*}/{vsn, \"$1\"}/g" src/meck.app.src
+git add src/meck.app.src
 
-echo git commit -m "Version $1"
-
-echo git tag -s $1 -m "Version $
+# Commit, tag and push
+git commit -m "Version $1"
+git tag -s $1 -m "Version $
 
 $CHANGELOG"
-
-echo git push && echo git push --tags
+git push && git push --tags
