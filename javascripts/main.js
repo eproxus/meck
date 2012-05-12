5f19159a0763d435a34fcceea0985dc2052bc947
Adam Lindberg <hello@alind.io>
2012-05-12 18:07:14 +0200

    Actually fix the rotating issue on iOS

--- javascripts/main.js
+++ javascripts/main.js
@@ -51,13 +51,4 @@ fixScale = function(doc) {
     doc[addEvent](type, fix, true);
   }
 
-  if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-    var viewportmeta = document.querySelector('meta[name="viewport"]');
-    if (viewportmeta) {
-        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
-        document.body.addEventListener('gesturestart', function () {
-            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-        }, false);
-    }
-  }
 };
