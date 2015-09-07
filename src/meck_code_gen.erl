52b7a3fbb2f481442e3c0208b1eab7c11b310da7
soranoba <soranoba@gmail.com>
2015-09-07 15:44:10 +0900

    FIX: optional_callbacks (but, it isn't the best practice)

--- src/meck_code_gen.erl
+++ src/meck_code_gen.erl
@@ -63,7 +63,7 @@ attributes(Mod) ->
     try
         [?attribute(Key, Val) || {Key, Val} <-
             proplists:get_value(attributes, Mod:module_info(), []),
-            Key =/= vsn, Key =/= deprecated]
+            Key =/= vsn, Key =/= deprecated, Key =/= optional_callbacks]
     catch
         error:undef -> []
     end.
